85b3251fdb51777c13ddf718e0998ebd17cf6b22
Sergio Rosa <scarlosjazz@gmail.com>
2019-03-24 10:34:23 -0400

    Resource modeling

--- src/main/java/com/sergiorosa/sergiorosafullstackappangularspringboot/SergiorosaFullStackAppAngularSpringBootApplication.java
+++ src/main/java/com/sergiorosa/sergiorosafullstackappangularspringboot/SergiorosaFullStackAppAngularSpringBootApplication.java
@@ -1,9 +1,13 @@
 package com.sergiorosa.sergiorosafullstackappangularspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class SergiorosaFullStackAppAngularSpringBootApplication {
 
 	public static void main(String[] args) {
