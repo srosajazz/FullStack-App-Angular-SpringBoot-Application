b46a6f1b944170cac8fb6d236f0624f5f08d44c1
Sergio Rosa <scarlosjazz@gmail.com>
2019-03-24 10:07:26 -0400

    Project Set-up

---
+++ src/main/java/com/sergiorosa/sergiorosafullstackappangularspringboot/SergiorosaFullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.sergiorosafullstackappangularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergiorosaFullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergiorosaFullStackAppAngularSpringBootApplication.class, args);
+	}
+
+}
